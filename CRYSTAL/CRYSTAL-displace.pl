e8fcad8717cc9271389bc8f1587edde3d04833cc
alexandr-fonari <tree@tr33.local>
2012-11-26 16:53:55 -0500

    testing.

--- CRYSTAL/CRYSTAL-displace.pl
+++ CRYSTAL/CRYSTAL-displace.pl
@@ -4,6 +4,17 @@ use strict;
 use warnings;
 use Data::Dumper;
 
+# physical constants in eV, Ang and s
+use constant PI => 4 * atan2(1, 1);
+use constant PlanckConstant => 4.135667516e-15; # [eV s]
+use constant HBAR => PlanckConstant/(2*PI); # [eV s]
+use constant CL => 2.99792458e18;    # [A/s]
+use constant AM => 931.494043e6;
+use constant Angstrom => 1.0e-10;    # [m]
+use constant EV => 1.60217733e-19;   # [J]
+use constant AMU => 1.6605402e-27;   # [kg]
+use constant CM2EV => 0.0001239842573148; # [eV]
+
 if ( scalar( @ARGV ) < 1 )
 {
     die( "\nUse: $0 <freq.out>\n" );
@@ -104,31 +115,31 @@ open( my $poscar_fh, ">", "DISPCAR" ) || die "Can't open DISPCAR file: $!";
 my @displacements = (-2, -1, 1, 2); # hard-coded so far for 5-point stencil finite difference 1st deriv.
 for( my $i = 0; $i < scalar(@e_values); $i++)
 {
-    print "processing ".($i+1)." out ".scalar(@e_values)." eigenvalues\n";
-    my $ev = $e_values[$i]*(-1.0);
+    #print "processing ".($i+1)." out ".scalar(@e_values)." eigenvalues\n";
+    my $ev = $e_values[$i];
     if($ev < 0.0){next;} # skip imaginary frequency
 
-    my $qi0 = sqrt((HBAR*CL)**2/(AM*sqrt($ev)*VaspToEv)); # mode quanta
+    my $qi0 = sqrt((HBAR*CL)**2/(AM*$ev*CM2EV)); # mode quanta
     # printf("%15.12f %15.12f\n", sqrt($ev)*VaspToEv, $qi0);
 
     my @disps = split('\s+', trim($e_vectors[$i]));
 
     foreach (@displacements)
     {
-        print $poscar_fh sprintf("POSCAR: disp=%d, w=%8.5f meV, qi0=%5e\n", $_, sqrt($ev)*VaspToEv*1000, $qi0);
+        print $poscar_fh sprintf("POSCAR: disp=%d, w=%8.5f meV, qi0=%5e\n", $_, $ev*CM2EV, $qi0);
         print $poscar_fh "1.00000\n";
         print $poscar_fh sprintf("%15.12f %15.12f %15.12f\n", $f[1], $f[2], $f[3]);
         print $poscar_fh sprintf("%15.12f %15.12f %15.12f\n", $f[4], $f[5], $f[6]);
         print $poscar_fh sprintf("%15.12f %15.12f %15.12f\n", $f[7], $f[8], $f[9]);
         print $poscar_fh join(" ", uniq(@a_labels))."\n";
-        print $poscar_fh join(" ", @a_count)."\n";
+        print $poscar_fh join(" ", count_uniq(@a_labels))."\n";
         print $poscar_fh "Cartesian\n";
 
         for( my $j = 0; $j < scalar(@a_cart_pos_x); $j++)
         {
             my $sqrtm = sqrt($a_masses[$j]);
             my($dx, $dy, $dz) = ($disps[3*$j]*$qi0*$_/$sqrtm, $disps[3*$j+1]*$qi0*$_/$sqrtm, $disps[3*$j+2]*$qi0*$_/$sqrtm);
-            print $poscar_fh sprintf("%15.12f %15.12f %15.12f\n", $a_cart_pos_x[$j]+$dx, $a_cart_pos_y[$j]+$dy, $a_cart_pos_z[$j]+$dz);
+            print $poscar_fh sprintf("%s %15.12f %15.12f %15.12f\n", $a_labels[$j], $a_cart_pos_x[$j]+$dx, $a_cart_pos_y[$j]+$dy, $a_cart_pos_z[$j]+$dz);
         }
         print $poscar_fh "\n";
     }
@@ -166,3 +177,9 @@ sub m3by1(@)
 }
 
 sub trim{ my $s=shift; $s =~ s/^\s+|\s+$//g; return $s;}
+
+# http://stackoverflow.com/questions/10193750/perl-count-unique-elements-in-array
+# consistent counting
+sub uniq {my %c; my @u = grep !$c{$_}++, @_; keys(%c);}
+sub count_uniq {my %c; my @u = grep !$c{$_}++, @_; values(%c);}
+
