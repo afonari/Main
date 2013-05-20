5e2644408a5242cadaed5d0449ad276fb2934fe6
Alexandr Fonari <pie@Nan.local>
2013-05-20 17:34:03 -0400

    .

--- VASP/VASP-displace.pl
+++ VASP/VASP-displace.pl
@@ -7,7 +7,7 @@
 # Version 0.99
 #
 # ===== ## =====
-# This is script is a fork of:
+# This script is a fork of:
 #
 #   Program: xdatgen
 #   Sung Sakong, PhD
@@ -15,7 +15,7 @@
 #   ver 0.5  4. June 2007
 #   url: http://www.uni-due.de/~hp0058/vmdplugins/utilities/xdatgen.c
 #
-# crystallographic conversions are heavily based on (some SUBs are copy/pasted):
+# Crystallographic conversions are heavily based on (some SUBs are copy/pasted):
 # http://theory.cm.utexas.edu/vasp/scripts/src/Vasp.pm
 # ===== ## =====
 #
@@ -27,7 +27,7 @@ use warnings;
 use XML::Simple;
 use Data::Dumper;
 
-# physical constants in eV, Ang and s
+# physical constants
 use constant PI => 4 * atan2(1, 1);
 use constant PlanckConstant => 4.135667516e-15; # [eV s]
 use constant HBAR => PlanckConstant/(2*PI); # [eV s]
@@ -38,19 +38,28 @@ use constant EV => 1.60217733e-19;   # [J]
 use constant AMU => 1.6605402e-27;   # [kg]
 use constant VaspToEv => sqrt(EV/AMU)/Angstrom/(2*PI)*PlanckConstant; # [eV] 6.46541380e-2
 use constant VaspToCm =>  VaspToEv/(1.2398419e-4); # [cm^-1] 521.47083
+
 my $xml = new XML::Simple();
 my $data = $xml->XMLin("vasprun.xml");
 
-# atomic masses and labels
-my (@a_masses, @a_labels, @a_count);
+my (@a_labels, @a_indx, @a_masses, @a_count, %label_mass);
+
 my @t = @{$data->{"atominfo"}->{"array"}->{"atomtypes"}->{"set"}->{"rc"}};
 foreach (@t)
 {
-    push(@a_masses, (trim($_->{"c"}[2])) x trim($_->{"c"}[0]));
-    push(@a_labels, (trim($_->{"c"}[1])) x trim($_->{"c"}[0]));
-    push(@a_count, trim($_->{"c"}[0]));
+    my ($label, $mass) = ( trim($_->{"c"}[1]), trim($_->{"c"}[2]) );
+    $label_mass{$label} = $mass;
 }
-# print Dumper( @a_masses, @a_labels, @a_count );
+# print Dumper( \% label_mass);
+
+@t = @{$data->{"atominfo"}->{"array"}->{"atoms"}->{"set"}->{"rc"}};
+foreach (@t)
+{
+    push(@a_labels, trim($_->{"c"}[0]));
+    push(@a_indx,   trim($_->{"c"}[1]));
+    push(@a_masses, $label_mass{trim($_->{"c"}[0])});
+}
+# print Dumper( @a_labels, @a_indx, @a_masses );
 
 # basis vectors
 my $basis;
@@ -66,8 +75,8 @@ for (my $i=0; $i<3; $i++)
 }
 # print Dumper( \%basis );
 
-# fractional atomic positions
-my ($coord_frac, $natoms);
+# fractional atomic positions and # of atoms
+my ($coord_frac, $coord_cart, $natoms);
 
 @t = @{$data->{"structure"}->{"initialpos"}->{"varray"}->{"v"}};
 $natoms = scalar(@t);
@@ -107,24 +116,23 @@ for( my $i = 0; $i < scalar(@e_values); $i++)
 
     foreach (@displacements)
     {
-        print $poscar_fh sprintf("POSCAR: disp=%d, w=%8.5f meV, qi0=%5e\n", $_, sqrt($ev)*VaspToEv*1000, $qi0);
+        print $poscar_fh sprintf("POSCAR: disp=%d, w=%8.5f meV\n", $_, sqrt($ev)*VaspToEv*1000);
         print $poscar_fh "1.00000\n";
-        print $poscar_fh sprintf("%15.12f %15.12f %15.12f\n", $f[1], $f[2], $f[3]);
-        print $poscar_fh sprintf("%15.12f %15.12f %15.12f\n", $f[4], $f[5], $f[6]);
-        print $poscar_fh sprintf("%15.12f %15.12f %15.12f\n", $f[7], $f[8], $f[9]);
-        print $poscar_fh join(" ", uniq(@a_labels))."\n";
-        print $poscar_fh join(" ", @a_count)."\n";
+        print $poscar_fh sprintf("%15.12f %15.12f %15.12f\n", $basis->[0][0], $basis->[1][0], $basis->[2][0]);
+        print $poscar_fh sprintf("%15.12f %15.12f %15.12f\n", $basis->[0][1], $basis->[1][1], $basis->[2][1]);
+        print $poscar_fh sprintf("%15.12f %15.12f %15.12f\n", $basis->[0][2], $basis->[1][2], $basis->[2][2]);
+        print $poscar_fh "_VASP SPECIFIC ATOM LABELS_\n";
+        print $poscar_fh "_VASP SPECIFIC ATOM COUNTS_\n";
         print $poscar_fh "Cartesian\n";
 
-        for( my $j = 0; $j < scalar(@a_cart_pos_x); $j++)
+        for( my $j = 0; $j < $natoms; $j++)
         {
             my $sqrtm = sqrt($a_masses[$j]);
             my($dx, $dy, $dz) = ($disps[3*$j]*$qi0*$_/$sqrtm, $disps[3*$j+1]*$qi0*$_/$sqrtm, $disps[3*$j+2]*$qi0*$_/$sqrtm);
-            print $poscar_fh sprintf("%15.12f %15.12f %15.12f\n", $a_cart_pos_x[$j]+$dx, $a_cart_pos_y[$j]+$dy, $a_cart_pos_z[$j]+$dz);
+            print $poscar_fh sprintf("%15.12f %15.12f %15.12f\n", $coord_cart->[$j][0]+$dx, $coord_cart->[$j][1]+$dy, $coord_cart->[$j][2]+$dz);
         }
         print $poscar_fh "\n";
     }
-    
 }
 
 print "DISPCAR created\n";
@@ -133,7 +141,7 @@ close($poscar_fh);
 sub trim{ my $s=shift; $s =~ s/^\s+|\s+$//g; return $s;}
 
 # http://stackoverflow.com/questions/439647/how-do-i-print-unique-elements-in-perl-array
-sub uniq {local %_; grep {!$_{$_}++} @_}
+# sub uniq {local %_; grep {!$_{$_}++} @_}
 
 sub dirkar {
     my $vector = shift;
